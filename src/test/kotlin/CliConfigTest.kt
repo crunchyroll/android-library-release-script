8cd92e08797e9699a22e29a622e677f9ef9769a6
Egor Neliuba <eneliuba@gmail.com>
2018-11-25 23:27:41 +0200

    Change CliConfig test name
    Jira: CXAND-4328

--- src/test/kotlin/CliConfigTest.kt
+++ src/test/kotlin/CliConfigTest.kt
@@ -2,7 +2,7 @@ import org.junit.Test
 
 class CliConfigTest {
     @Test(expected = IllegalArgumentException::class)
-    fun test() {
+    fun `--no-tag-prefix flag and --tag-prefix argument cannot be used together`() {
         CliConfig.parse(arrayOf("--no-tag-prefix", "--tag-prefix", "v"))
     }
 }
