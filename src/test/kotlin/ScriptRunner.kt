4aeef87764c1f6d73d00ba4623a09b150637d989
Egor Neliuba <eneliuba@gmail.com>
2018-12-17 21:48:33 +0200

    Print errors in String#output
    Jira: CXAND-4328

--- src/test/kotlin/ScriptRunner.kt
+++ src/test/kotlin/ScriptRunner.kt
@@ -5,10 +5,12 @@ fun String.output(workingDir: File = File(".")): String {
     println("Running command: $this")
     with(ProcessBuilder(split(" ")).directory(workingDir).start()) {
         waitFor()
+        val output = inputStream.bufferedReader().use(BufferedReader::readText).trim()
         val exitValue = exitValue()
         if (exitValue != 0) {
+            println(output)
             throw IllegalStateException("Command $this has exited with non-zero value $exitValue.")
         }
-        return inputStream.bufferedReader().use(BufferedReader::readText).trim()
+        return output
     }
 }
