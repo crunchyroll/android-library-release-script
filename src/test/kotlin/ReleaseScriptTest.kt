787e682c2f0a9c204261f5315d313e679e859638
Egor Neliuba <eneliuba@gmail.com>
2018-12-17 21:33:28 +0200

    Assert there are no file changes in --dry-run mode
    Jira: CXAND-4328

--- src/test/kotlin/ReleaseScriptTest.kt
+++ src/test/kotlin/ReleaseScriptTest.kt
@@ -1,38 +1,38 @@
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
+
+private val changelogContent = """
+                            # Changelog
+
+                            ## Version 1.0.1 *(In development)*
+
+                            - Another fake changelog entry
+
+                            ## Version 1.0.0 *(2018-11-28)*
+
+                            - Fake changelog entry
+                            """.trimIndent()
+
+private val gradlePropertiesContent = """
+                            VERSION_NAME=1.0.1
+                            """.trimIndent()
 
 class ReleaseScriptTest {
     @get:Rule
     val script = TemporaryReleaseScript()
 
     @Test
-    fun `script is runnable in --dry-run mode`() {
-        val a = script.newFile("gradle.properties").apply {
-            writeText(
-                """
-            VERSION_NAME=1.0.1
-            """.trimIndent()
-            )
-        }
-        val b = script.newFile("CHANGELOG.md").apply {
-            writeText(
-                """
-            # Changelog
-
-            ## Version 1.0.1 *(In development)*
-
-            - Another fake changelog entry
-
-            ## Version 1.0.0 *(2018-11-28)*
-
-            - Fake changelog entry
-            """.trimIndent()
-            )
-        }
+    fun `doesn't change file contents in --dry-run mode`() {
+        val gradleProperties = script.newFile("gradle.properties")
+        gradleProperties.writeText(gradlePropertiesContent)
+        val changelog = script.newFile("CHANGELOG.md")
+        changelog.writeText(changelogContent)
         script.newFile("gradlew")
+
         script.run("--dry-run")
 
-        println(a.readText())
-        println(b.readText())
+        assertEquals(gradlePropertiesContent, gradleProperties.readText())
+        assertEquals(changelogContent, changelog.readText())
     }
 }
